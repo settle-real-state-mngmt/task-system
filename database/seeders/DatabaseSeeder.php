3ff54a5a605eec7bd0bf189647a7ed606d157a47
Bruno <brunobraga@protonmail.com>
2025-02-28 13:10:40 -0300

    Add doc block and status from Response class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Alan Doe',
+            'email' => 'alandoe@example.org',
+            'password' => 'password',
         ]);
     }
 }
--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
 
 uses(RefreshDatabase::class);
 
@@ -10,28 +11,41 @@ $fakeUser = [
     'password' => 'password'
 ];
 
+/**
+ * Check if /register is sign up users properly
+ */
 test('if /register returns 201 when valid data is sent', function () use ($fakeUser) {
     $response = $this->post('/api/register', $fakeUser, ['accept' => 'application/json']);
+    $response->assertStatus(Response::HTTP_CREATED);
     $response->assertJsonStructure([
         'message',
         'data' => ['id', 'name', 'email', 'created_at', 'updated_at'],
     ]);
 });
 
+/**
+ * Check if api is not allowing users to sign up same email
+ */
 test('if /register returns 422 when saving same email twice', function () use ($fakeUser) {
     $response = $this->post('/api/register', $fakeUser, ['accept' => 'application/json']);
     $response = $this->post('/api/register', $fakeUser, ['accept' => 'application/json']);
-    $response->assertStatus(422);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
 });
 
+/**
+ * Check if name has no more than 30 characters
+ */
 test('if /register returns 422 when passing more than 30 char to name', function () use ($fakeUser) {
     $fakeUser['name'] = 'thisisanamethatdefinitelyhasmorethan30chars';
     $response = $this->post('/api/register', $fakeUser, ['accept' => 'application/json']);
-    $response->assertStatus(422);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
 });
 
+/**
+ * Check if password has at least 8 characters
+ */
 test('if /register returns 422 when passing less than 8 char to password', function () use ($fakeUser) {
     $fakeUser['password'] = '1';
     $response = $this->post('/api/register', $fakeUser, ['accept' => 'application/json']);
-    $response->assertStatus(422);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
 });
