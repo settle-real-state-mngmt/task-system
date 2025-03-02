7a869b67d82c7bb3a07721151d463023348a80d9
Bruno <brunobraga@protonmail.com>
2025-03-02 19:59:42 -0300

    Handle common global exceptions at app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +22,11 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (AuthenticationException $e, Request $req) {
+            return response()->json([], 401);
+        });
+
+        $exceptions->render(function (QueryException $e, Request $req) {
+            return response()->json($e, 500);
+        });
     })->create();
