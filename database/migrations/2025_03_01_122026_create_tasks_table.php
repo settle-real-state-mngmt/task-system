e0a3eaef57999fafd39c2575bb21ced686b9acbb
Bruno <brunobraga@protonmail.com>
2025-03-03 21:16:39 -0300

    Add status column on tasks

--- database/migrations/2025_03_01_122026_create_tasks_table.php
+++ database/migrations/2025_03_01_122026_create_tasks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
+            $table->enum('status', ['Open', 'In Progress', 'Completed', 'Rejected']);
             $table->foreignId('building_id')->constrained();
             $table->foreignId('user_id')->constained();
             $table->timestamps();
