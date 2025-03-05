1482695b8961f6d1d82a76ede6983959d14b8281
Bruno <brunobraga@protonmail.com>
2025-03-04 21:24:10 -0300

    Add php doc blocks

--- tests/Feature/TaskControllerTest.php
+++ tests/Feature/TaskControllerTest.php
@@ -10,7 +10,7 @@ beforeEach(function () {
 });
 
 /**
- *
+ * Checks if a user logged as owner of a building can comment on a task
  */
 test('Test if team/building owner can comment on a task of a owned building', function () {
     $response = $this->loginAsOwner()->post(
@@ -21,6 +21,9 @@ test('Test if team/building owner can comment on a task of a owned building', fu
     $response->assertOk();
 });
 
+/**
+ * Checks if a user logged as a team member of a building can comment on a task
+ */
 test('Test if team member can comment on a task of a building that he/she works', function () {
     $response = $this->loginAsStaff()->post(
         strtr(ApiUrls::ADD_COMMENT_TO_TASK, [':id' => Fakes::TASK_ID]),
@@ -30,6 +33,9 @@ test('Test if team member can comment on a task of a building that he/she works'
     $response->assertOk();
 });
 
+/**
+ * Checks if a user logged that is not a team member of a building can comment on a task
+ */
 test('Test if user that is not from a team can comment on a task', function () {
     $response = $this->loginAsNonStaff()->post(
         strtr(ApiUrls::ADD_COMMENT_TO_TASK, [':id' => Fakes::TASK_ID]),
